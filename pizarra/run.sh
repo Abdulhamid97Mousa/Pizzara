f955efc6ba8d1cc4c16ec928d3d8d20bc9cc26a0
Abdulhamid97Mousa <abdulhamid97mouse@buaa.edu.cn>
2025-01-26 03:48:17 +0800

    add: added run.sh

--- pizarra/run.sh
+++ pizarra/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# Local development setup
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify installation
+docker-compose --version
+
+# Build and start containers
 docker-compose build --no-cache && \
-docker-compose up 
+docker-compose up
